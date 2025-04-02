3efbb6b067a31d5396f5a985dbc985610b0a272d
keynguyenB <nguyenminhvinhky@utexas.edu>
2025-04-02 16:26:38 -0500

    Update fix-phone-num.py

--- fix-phone-num.py
+++ fix-phone-num.py
@@ -1,21 +1,27 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
-    if len(phone_num_to_fix) == 11 and phone_num_to_fix[0] == "1":
-        phone_num_to_fix = phone_num_to_fix[1:]  # Remove the country code "1"
+    
+    if len(phone_num_to_fix) == 11:
+        # The phone number is of length 11, so check if it has the "1" country code.
+        if phone_num_to_fix[0] != "1":
+            # The country code wasn't a "1", so the number is invalid.
+            raise ValueError(f"phone numbers of length 11 must begin with a 1, got \"{phone_num_to_fix}\"")
+        # Strip off the country code "1" from the beginning.
+        # Resulting string should be of length 10 to work with formatting code below.
+        phone_num_to_fix = phone_num_to_fix[1:]
 
     if len(phone_num_to_fix) != 10:
         raise ValueError(f"phone number must be length 10; got \"{phone_num_to_fix}\" which is of length {len(phone_num_to_fix)}")
-
     if not phone_num_to_fix.isdigit():
         raise ValueError(f"phone number must only contain digits; got \"{phone_num_to_fix}\"")
-
+    
     # given "5125558823". Split the parts, then recombine and return
     area_code = phone_num_to_fix[0:3]  # 512 (first three digits)
     three_part = phone_num_to_fix[3:6]  # 555 (next three digits)
-    four_part = phone_num_to_fix[6:]  # 8823 (last four digits)
+    four_part = phone_num_to_fix[6:]  # # 8823 (last four digits)
 
-    fixed_num = f"({area_code}) {three_part} {four_part}"
+    fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part
 
     return fixed_num
 
@@ -30,20 +36,20 @@ def test_fix_phone_num3():
     assert fix_phone_num("3216543333") == '(321) 654 3333'
 
 def test_short_num_error():
-    with pytest.raises(ValueError):
-        fix_phone_num("51")
+  with pytest.raises(ValueError):
+    fix_phone_num("51")
 
 def test_long_num_error():
-    with pytest.raises(ValueError):
-        fix_phone_num("01234567890")
+  with pytest.raises(ValueError):
+    fix_phone_num("01234567890")
 
 def test_non_digit_error():
-    with pytest.raises(ValueError):
-        fix_phone_num("012345678a")
+  with pytest.raises(ValueError):
+    fix_phone_num("012345678a")
 
 def test_fix_phone_num_country_code():
     assert fix_phone_num("15125558823") == '(512) 555 8823'
 
 def test_wrong_country_code():
     with pytest.raises(ValueError):
-        fix_phone_num("25125558823")
+        assert fix_phone_num("25125558823")
